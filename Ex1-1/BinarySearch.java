febc483ba69ddcbcb5ec53c8e8200ca79d2799a5
ZhaoPu <zhaopuwp@outlook.com>
2015-07-27 10:14:15 +0800

    Initial commit

---
+++ Ex1-1/BinarySearch.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+/**
+ * Created by pu on 2015/7/27.
+ */
+public class BinarySearch {
+    public static int rank(int key,int[] a){
+        int lo=0;
+        int hi=a.length-1;
+        while(lo<=hi){
+            int mid=(lo+hi)/2;
+            if(a[mid]==key)
+                return mid;
+            if(a[mid]<key)
+                lo=mid+1;
+            if(a[mid]>key)
+                hi=mid-1;
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers={1,5,7,8,6,4,10};
+        Arrays.sort(numbers);
+        System.out.println(rank(7,numbers));
+    }
+}
