708b956dea79a75782a4d79790b985eb51ee5043
ZhaoPu <zhaopuwp@outlook.com>
2015-07-28 10:06:44 +0800

    Examples: Counter

---
+++ Ex1-2/Counter.java
@@ -0,0 +1,19 @@
+/**
+ * Created by pu on 2015/7/28.
+ */
+public class Counter {
+    private final String name;
+    private int count;
+    Counter(String id){
+        name=id;
+    }
+    public void increment(){
+        count++;
+    }
+    public int tally(){
+        return count;
+    }
+    public String toString(){
+        return count+" "+name;
+    }
+}
