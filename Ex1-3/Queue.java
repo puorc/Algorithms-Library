2099865d2c8905739a2def3dc366b2d739c476d7
ZhaoPu <zhaopuwp@outlook.com>
2015-07-30 18:00:51 +0800

    Queue

---
+++ Ex1-3/Queue.java
@@ -0,0 +1,55 @@
+/**
+ * Created by ZhaoPu on 2015/7/30.
+ */
+import java.util.Iterator;
+public class Queue<Item> implements Iterable<Item> {
+    private node first;
+    private node last;
+    private class node{
+        Item item;
+        node next;
+    }
+    private int N;
+    public Queue(){}
+    public Queue(Queue<Item> a ){
+        node current=a.getFirst();
+        while(current!=null){
+            this.enqueue(current.item);
+            current=current.next;
+        }
+    }
+    public node getFirst(){ return first;}
+    public boolean isEmpty(){ return N==0; }
+    public int size(){ return  N; }
+    public void enqueue(Item item){
+        node oldlast=last;
+        last=new node();
+        last.item=item;
+        last.next=null;
+        if(oldlast==null){
+            first=last;
+        }
+        else{
+            oldlast.next=last;
+        }
+        N++;
+    }
+    public Item dequeue(){
+        Item tmp=first.item;
+        if(N==1) first = last = null;
+        else first=first.next;
+        N--;
+        return tmp;
+    }
+    public Iterator<Item> iterator(){ return new QueueIterator(); }
+    private class QueueIterator implements Iterator<Item>{
+        private node current=first;
+        public boolean hasNext(){ return current!=null; }
+        public void remove(){}
+        public Item next(){
+            Item tmp=current.item;
+            current=current.next;
+            return tmp;
+        }
+    }
+}
