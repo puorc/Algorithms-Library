1de150379c02749179fdf8ed6264205e9f1827bb
ZhaoPu <zhaopuwp@outlook.com>
2015-07-31 12:52:15 +0800

    Random bag

---
+++ Ex1-3/RandomBag.java
@@ -0,0 +1,68 @@
+import java.util.Iterator;
+
+/**
+ * Created by ZhaoPu on 2015/7/30.
+ */
+import java.util.Random;
+import java.util.Iterator;
+public class RandomBag<Item> implements Iterable<Item> {
+    private Node first;
+    private int N;
+    private class Node{
+        Item item;
+        Node next;
+    }
+    public void add(Item item){
+        Node oldfirst=first;
+        first=new Node();
+        first.item=item;
+        first.next=oldfirst;
+        N++;
+    }
+    public Iterator<Item> iterator(){
+        return new ListIterator();
+    }
+    private class ListIterator implements Iterator<Item>{
+        private Node current=first;
+        private Item[] a=(Item[])new Object[N];
+        private int[] random=new int[N];
+        public ListIterator(){
+            for(int i=0;i<N;i++){
+                a[i]=current.item;
+                current=current.next;
+            }
+        }
+        public void remove(){}
+        public boolean hasNext(){
+            for(int i=0;i<N;i++)
+                if(random[i]==0)
+                    return true;
+            return false;
+        }
+        private int Random(int min,int max){
+            Random rand=new Random();
+            int randomNum = rand.nextInt((max - min) + 1) + min;
+            return randomNum;
+        }
+        public Item next(){
+            int theRandom=Random(0,N-1);
+            while(random[theRandom]!=0)
+                theRandom=Random(0,N-1);
+            random[theRandom]=1;
+            return a[theRandom];
+        }
+    }
+      /*  private Node current=first
+        public boolean hasNext(){
+            return current!=null;
+        }
+        public void remove(){}
+        public Item next(){
+            Item item=current.item;
+            current=current.next;
+            return item;
+        }
+    }     */
+    public boolean isEmpty(){ return N==0; }
+    public int size(){ return N ;}
+}
