92f3aa6efe7389791ba0c9af4fbc8b0764a41891
ZhaoPu <zhaopuwp@outlook.com>
2015-08-01 16:49:41 +0800

    Parentheses match

---
+++ Ex1-3/Parentheses.java
@@ -0,0 +1,26 @@
+/**
+ * Created by ZhaoPu on 2015/8/1.
+ */
+public class Parentheses {
+    public static boolean isComplete(char[] c){
+        Stack<Character> stack=new Stack<>();
+        for(int i=0;i<c.length;i++){
+            if(c[i]=='('||c[i]=='{'||c[i]=='[')
+                stack.push(c[i]);
+            else if(c[i]==')'){
+                if(stack.pop()!='(')
+                    return false;
+            }
+            else if(c[i]=='}'){
+                if(stack.pop()!='{')
+                    return false;
+            }
+            else if(c[i]==']')
+                if(stack.pop()!='[')
+                    return false;
+            else
+                    return stack.isEmpty();
+        }
+        return stack.isEmpty();
+    }
+}
