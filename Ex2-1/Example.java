92863f76c763e24baf22b0fffeced44427c2fccd
ZhaoPu <zhaopuwp@outlook.com>
2015-08-04 19:30:34 +0800

    Abstract sort

---
+++ Ex2-1/Example.java
@@ -0,0 +1,23 @@
+/**
+ * Created by pu on 2015/8/4/004.
+ */
+public abstract class Example {
+    public abstract void sort(Comparable[] a);
+    private boolean less(Comparable a,Comparable b){
+        return a.compareTo(b)<0;
+    }
+    private void exch(Comparable[] a,int i,int j){
+        Comparable t=a[i];
+        a[i]=a[j];
+        a[j]=t;
+    }
+    public void show(Comparable[] a){
+        for(int i=0;i<a.length;i++)
+            System.out.println(a[i]);
+    }
+    public boolean isSorted(Comparable[] a){
+        for(int i=0;i<a.length-1;i++)
+            if(less(a[i+1],a[i])) return false;
+        return true;
+    }
+}
