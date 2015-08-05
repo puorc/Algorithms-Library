3e7ac68d2c7082942b49d5e10f1e75bdc60b92b4
ZhaoPu <zhaopuwp@outlook.com>
2015-08-05 10:32:39 +0800

    Insertion sort

---
+++ Ex2-1/Insertion.java
@@ -0,0 +1,12 @@
+/**
+ * Created by pu on 2015/8/5/005.
+ */
+public class Insertion extends Example{
+    public void sort(Comparable[] a){
+        for(int i=0;i<a.length-1;i++){
+            for(int j=i;j>=0&&less(a[j+1],a[j]);j--){
+                exch(a,j,j+1);
+            }
+        }
+    }
+}
