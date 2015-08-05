584ac69c63ac084579f04907ce5bd637ccd67009
ZhaoPu <zhaopuwp@outlook.com>
2015-08-05 10:21:05 +0800

    Selection sort

---
+++ Ex2-1/Selection.java
@@ -0,0 +1,16 @@
+/**
+ * Created by pu on 2015/8/4/004.
+ */
+public class Selection extends Example {
+    public void sort(Comparable[] a){
+        for(int i=0;i<a.length;i++){
+            Comparable min=a[i];
+            for(int j=i+1;j<a.length;j++){
+                if(less(a[j],min)){
+                    min=a[j];
+                    exch(a,i,j);
+                }
+            }
+        }
+    }
+}
