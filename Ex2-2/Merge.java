f9fc7f4354446dd17b6e427724c1cdc018172e4e
ZhaoPu <zhaopuwp@outlook.com>
2015-08-07 15:48:14 +0800

    Rid of static array.

--- Ex2-2/Merge.java
+++ Ex2-2/Merge.java
@@ -2,17 +2,17 @@
  * Created by pu on 2015/8/6/006.
  */
 public class Merge {
-    private static Comparable[] aux;
 
     private static boolean less(Comparable i, Comparable j) {
         return i.compareTo(j) < 0;
     }
 
-    private static void merge(Comparable[] a, int lo, int mid, int hi) {
+    private static void merge(Comparable[] a, Comparable[] aux, int lo, int mid, int hi) {
         int i = lo;
         int j = mid + 1;
-        for (int k = lo; k <= hi; k++)
+        for (int k = lo; k <= hi; k++) {
             aux[k] = a[k];
+        }
         for (int k = lo; k <= hi; k++) {
             if (i > mid) a[k] = aux[j++];
             else if (j > hi) a[k] = aux[i++];
@@ -22,15 +22,15 @@ public class Merge {
     }
 
     public static void sort(Comparable[] a) {
-        aux = new Comparable[a.length];
-        sort(a, 0, a.length - 1);
+        Comparable[] aux = new Comparable[a.length];
+        sort(a, aux, 0, a.length - 1);
     }
 
-    private static void sort(Comparable[] a, int lo, int hi) {
+    private static void sort(Comparable[] a, Comparable[] aux, int lo, int hi) {
         if (lo >= hi) return;
         int mid = (lo + hi) / 2;
-        sort(a, lo, mid);
-        sort(a, mid + 1, hi);
-        merge(a, lo, mid, hi);
+        sort(a, aux, lo, mid);
+        sort(a, aux, mid + 1, hi);
+        merge(a, aux, lo, mid, hi);
     }
 }
