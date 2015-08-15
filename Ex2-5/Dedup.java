e3cfad369ae51630809eb79c6a125cbde6b8bfb7
ZhaoPu <zhaopuwp@outlook.com>
2015-08-15 16:24:02 +0800

    Ex2.5.4

--- Ex2-5/Dedup.java
+++ Ex2-5/Dedup.java
@@ -2,21 +2,21 @@
  * Created by pu on 2015/8/15/015.
  */
 public class Dedup {
-    public static String[] dedup(String[] a){
+    public static String[] dedup(String[] a) {
         Quick.sort(a);
-        Queue<String> tmp=new Queue<>();
-        String key=a[0];
+        Queue<String> tmp = new Queue<>();
+        String key = a[0];
         tmp.enqueue(key);
-        for(int i=1;i<a.length;i++){
-            if(a[i]!=key){
-                key=a[i];
+        for (int i = 1; i < a.length; i++) {
+            if (a[i] != key) {
+                key = a[i];
                 tmp.enqueue(key);
             }
         }
-        String[] result=new String[tmp.size()];
-        int index=0;
-        for(String i:tmp)
-            result[index++]=i;
+        String[] result = new String[tmp.size()];
+        int index = 0;
+        for (String i : tmp)
+            result[index++] = i;
         return result;
     }
 }
