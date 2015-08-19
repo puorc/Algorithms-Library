24e3e4e87fb7e58b172ece558453ce1abb825d71
ZhaoPu <zhaopuwp@outlook.com>
2015-08-19 19:35:26 +0800

    time calculation

--- Ex3-1/FrequencyCounter.java
+++ Ex3-1/FrequencyCounter.java
@@ -3,12 +3,15 @@
  */
 public class FrequencyCounter {
     public static void main(String[] args) {
-        SequentialSearchST<String, Integer> st = new SequentialSearchST<>();
+        int minlen = Integer.parseInt(args[0]);
+        ST<String, Integer> st = new SequentialSearchST<>();
+        Stopwatch watch = new Stopwatch();
         while (!StdIn.isEmpty()) {
             String word = StdIn.readString();
-            if (word.length() < 1) continue;
-            if (!st.contains(word)) st.put(word, 1);
-            else
+            if (word.length() < minlen) continue;
+            if (!st.contains(word)) {
+                st.put(word, 1);
+            } else
                 st.put(word, st.get(word) + 1);
         }
         String max = "";
@@ -17,6 +20,6 @@ public class FrequencyCounter {
             if (st.get(word) > st.get(max))
                 max = word;
         }
-        StdOut.println(max + " " + st.get(max));
+        StdOut.println(max + " " + st.get(max) + " time " + watch.elapsedTime() + "s");
     }
 }
